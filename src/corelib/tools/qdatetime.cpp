dcfd3f740830649582506192cbd17e9a5b3c9705
Pasi Petäjäjärvi <pasi.petajajarvi@theqtcompany.com>
2015-01-15 13:39:40 +0200

    Add qt_timezone implementation for VxWorks7
    Change-Id: I5a578ab9c61955439930c6330ed6dff8bb3c8dd2
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>

--- src/corelib/tools/qdatetime.cpp
+++ src/corelib/tools/qdatetime.cpp
@@ -2221,6 +2221,14 @@ static int qt_timezone()
         return -t.tm_gmtoff + (t.tm_isdst ? (long)SECS_PER_HOUR : 0L);
 #elif defined(Q_OS_INTEGRITY)
         return 0;
+#elif defined(Q_OS_VXWORKS)
+        struct timeval tv;
+        gettimeofday(&tv, 0);
+        time_t ltime = tv.tv_sec;
+        struct tm *t = 0;
+        struct tm res;
+        t = gmtime_r(&ltime, &res);
+        return (MSECS_PER_HOUR * t->tm_hour + MSECS_PER_MIN * t->tm_min + 1000 * t->tm_sec + tv.tv_usec / 1000);
 #else
         return timezone;
 #endif // Q_OS_WIN
@@ -2237,6 +2245,9 @@ static QString qt_tzname(QDateTimePrivate::DaylightStatus daylightStatus)
         return QString();
     return QString::fromLocal8Bit(name);
 #else
+#ifdef Q_OS_VXWORKS
+    const char *tzname[2] = { "std", "dst" };
+#endif
     return QString::fromLocal8Bit(tzname[isDst]);
 #endif // Q_OS_WIN
 }
