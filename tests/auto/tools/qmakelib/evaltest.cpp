9b20191353c4a15f087dccc6ca406ad3c4102da1
Zbigniew Chyla <zbigniew.chyla@siili.com>
2020-02-03 13:53:05 +0100

    Sam's merge: revert some changes in tests
    f5433f44ee ("Sam's vxworks 5.9.7 merge") reverted parts of the following
    commits that modify test files, without reverting other parts of these
    commits:
     - e0c798742a
    ("QObject: do allow setProperty() to change the type of the property")
     - 5a79a9ea41
    ("Make sure $$relative_path() uses an absolute path as its first arg")
     - a332937cde
    ("qmake: Add test functions for comparing version numbers")
     - 12a7513a5d
    ("Fix potential crash when showing line/paragraph separators")
     - 6670948fe3
    ("Make QPalette::setBrush() check before detaching")
     - be78baa165
    ("Fix crash if QPixmap::defaultDepth() is called when no QGuiApplication")
     - 1bf0344b9f
    ("Update CLDR to v31.0.1, test Costa Rican currency's fractional part")
     - 961b8f51a2
    ("QJsonDocument::fromRawData: Fix out-of-bounds access")
     - e0d0fd38d0
    ("QJsonDocument: Make emptyObject an object")
    
    Note:
    This commit has been added as the result of comparison between the head
    of our branch with vxworks changes rebuilt as linear history (rebased
    onto v5.9.7) and the "Sam's vxworks 5.9.7 merge" commit from the
    vxworks-5.9 branch.
    It contains changes introduced by the "Sam's vxworks 5.9.7 merge" in
    addition to "merging / rebasing onto v5.9.7". We included all these
    changes on our (linearized) branch so that heads of both branches are
    identical.
    
    Change-Id: I60aa5361e78d4417cbaf1f705abe5ca5bd77bf92

--- tests/auto/tools/qmakelib/evaltest.cpp
+++ tests/auto/tools/qmakelib/evaltest.cpp
@@ -2225,44 +2225,6 @@ void tst_qmakelib::addTestFunctions(const QString &qindir)
             << ""
             << true;
 
-    QTest::newRow("versionAtLeast(): true")
-            << "VAR = 1.2.3\nversionAtLeast(VAR, 1.2.3): OK = 1"
-            << "OK = 1"
-            << ""
-            << true;
-
-    QTest::newRow("versionAtLeast(): false")
-            << "VAR = 1.2.2\nversionAtLeast(VAR, 1.2.3): OK = 1"
-            << "OK = UNDEF"
-            << ""
-            << true;
-
-    QTest::newRow("versionAtLeast(): bad number of arguments")
-            << "versionAtLeast(1): OK = 1\nversionAtLeast(1, 2, 3): OK = 1"
-            << "OK = UNDEF"
-            << "##:1: versionAtLeast(var, version) requires two arguments.\n"
-               "##:2: versionAtLeast(var, version) requires two arguments."
-            << true;
-
-    QTest::newRow("versionAtMost(): true")
-            << "VAR = 1.2.3\nversionAtMost(VAR, 1.2.3): OK = 1"
-            << "OK = 1"
-            << ""
-            << true;
-
-    QTest::newRow("versionAtMost(): false")
-            << "VAR = 1.2.3\nversionAtMost(VAR, 1.2.2): OK = 1"
-            << "OK = UNDEF"
-            << ""
-            << true;
-
-    QTest::newRow("versionAtMost(): bad number of arguments")
-            << "versionAtMost(1): OK = 1\nversionAtMost(1, 2, 3): OK = 1"
-            << "OK = UNDEF"
-            << "##:1: versionAtMost(var, version) requires two arguments.\n"
-               "##:2: versionAtMost(var, version) requires two arguments."
-            << true;
-
     QTest::newRow("clear(): top-level")
             << "VAR = there\nclear(VAR): OK = 1"
             << "OK = 1\nVAR ="
