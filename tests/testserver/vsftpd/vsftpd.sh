c2f78c9b7c3e3e2f63cf5cecb2a471ab09decaf6
Pasi Petäjäjärvi <pasi.petajajarvi@qt.io>
2020-11-12 13:27:27 +0200

    Update docker networks servers configurations
    Change-Id: Ie016b28ed76b43c0857c7fdddbd495fe4d0867d6
    Reviewed-by: Teemu Holappa <teemu.holappa@qt.io>

--- tests/testserver/vsftpd/vsftpd.sh
+++ tests/testserver/vsftpd/vsftpd.sh
@@ -41,9 +41,10 @@ useradd -d "/home/$USER/ftp" -s /bin/bash ftptest; echo "ftptest:$PASS" | chpass
 cp $TESTDATA/vsftpd.{conf,user_list} /etc/
 
 # Resolve error message "vsftpd failed - probably invalid config" during boot
-command='start-stop-daemon --start --background -m --oknodo --pidfile /var/run/vsftpd/vsftpd.pid'
-command+=' --exec ${DAEMON}'
-sed -i "s,$command.*$,$command; sleep 1," /etc/init.d/vsftpd
+# This bug has been reported to Debian bug tracking system (ID #911396)
+command='ps -C vsftpd | grep -qs "${_PID}"'
+sed -i -e 's,while [ ${n} -le 5 ].*$,while true,' \
+    -e "s,\t\t\tif ! $command.*$,\t\t\tif $command," /etc/init.d/vsftpd
 
 # Populate the FTP sites:
 su $USER -c "cp -r $TESTDATA/ftp ~/ftp"
@@ -52,6 +53,10 @@ ln -s /home/$USER/ftp /var/ftp
 # tst_QNetworkReply::getFromFtp_data()
 su $USER -c "mkdir -p ~/ftp/qtest/"
 su $USER -c "cp rfc3252.txt ~/ftp/qtest/"; rm rfc3252.txt
+
+# tst_QNetworkReply::proxy_data()
+su $USER -c "ln ~/ftp/qtest/rfc3252.txt ~/ftp/qtest/rfc3252"
+su $USER -c "mkdir -p ~/ftp/qtest/nonASCII/"
 
 # Duplicate rfc3252.txt 20 times for bigfile tests:
 su $USER -c "seq 20 | xargs -i cat ~/ftp/qtest/rfc3252.txt >> ~/ftp/qtest/bigfile"
