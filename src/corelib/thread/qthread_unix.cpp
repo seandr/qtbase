d80283a857f42b0c482019086baeeebfdfe0af78
Thiago Macieira <thiago.macieira@intel.com>
2025-03-24 22:14:32 -0700

    QLibrary/Darwin: don't use RTLD_NODELETE, just don't dlclose()
    As the comment says, the OS does support the flag and a perusal of the
    source code for dyld[1] shows it does handle the flag, setting an
    internal flag called "leaveMapped", which is different from the
    "neverUnload" flag. My guess is that it literally just leaves the memory
    for the plugin mapped, but removes all links to it in the dyld internal
    data structures.
    
    That results in the Objective C runtime crashing when running content
    from NODELETE plugins that has been dlclose()d. This probably explains
    why lldb stops seeing those symbols too:
    
    (lldb) f
    frame #0: 0x0000000103872eb4 QtCore`QLibraryPrivate::unload_sys(this=0x000060000179c210) at qlibrary_unix.cpp:258:21
    -> 258      if (doTryUnload && dlclose(pHnd.loadAcquire())) {
    (lldb) target modules list libqcocoa.dylib
    [  0] 81EB58D5-8D31-333A-9E8C-F2385F6EFCF4 0x00000001043de000 libqcocoa.dylib
    (lldb) image lookup -vn QCocoaEventDispatcher::QCocoaEventDispatcher
    2 matches found in libqcocoa.dylib:
            Address: libqcocoa.dylib[0x0000000000036480] (libqcocoa.dylib.__TEXT.__text + 190368)
            Summary: libqcocoa.dylib`QCocoaEventDispatcher::QCocoaEventDispatcher(QObject*) at qcocoaeventdispatcher.mm:776
    ...
    (lldb) n
    
    (lldb) target modules list libqcocoa.dylib
    [  0] 81EB58D5-8D31-333A-9E8C-F2385F6EFCF4 libqcocoa.dylib[0x0000000000000000] libqcocoa.dylib
    (lldb) image lookup -vn QCocoaEventDispatcher::QCocoaEventDispatcher
    
    [1] https://github.com/apple-oss-distributions/dyld
    
    Task-number: QTBUG-135044
    Task-number: QTBUG-134080
    Task-number: QTBUG-133861
    Task-number: QTBUG-132697
    Task-number: QTBUG-102984
    Task-number: QTBUG-132381
    Change-Id: I7da3b3615a6ace7c72d1fffd5cf560d8f8a4e1bb
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 1a54a0330141ea01fa9b81c8deb23f063815ce8f)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 9565314141045e7059e045f058053f535a31ebcd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/thread/qthread_unix.cpp
+++ src/corelib/thread/qthread_unix.cpp
@@ -119,12 +119,12 @@ enum { ThreadPriorityResetFlag = 0x80000000 };
 // Finally, for the thread that called ::exit() (which in most cases happens by
 // returning from the main() function), finish() and cleanup() happen at
 // function-local static destructor time, and the deref & delete happens later,
-// at global static destruction time. This is important so QLibraryStore can
-// unload plugins between those two steps: it is also destroyed by a global
-// static destructor, but with a lower priority than ours. The order needs to
-// be this way so we delete the event dispatcher before plugins unload, as
-// often the dispatcher for the main thread is provided by a QPA plugin, but
-// the QThreadData object must still be alive when the plugins do unload.
+// at global static destruction time. That way, we delete the event dispatcher
+// before QLibraryStore's clean up runs and unloads remaining plugins. This
+// strategy was chosen because of crashes observed while running the event
+// dispatcher's destructor, and though the cause of the crash was something
+// else (QFactoryLoader always loads with PreventUnloadHint set), other plugins
+// may still attempt to access QThreadData in their global destructors.
 
 // Always access this through the {get,set,clear}_thread_data() functions.
 Q_CONSTINIT static thread_local QThreadData *currentThreadData = nullptr;
