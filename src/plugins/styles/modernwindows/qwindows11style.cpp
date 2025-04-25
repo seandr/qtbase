e1aff075a33e19bd619d52c62cd8d2f15b041219
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-04-25 22:01:31 +0200

    Windows11Style: draw background of item in an itemview
    The background was not drawn within drawControl(CE_ItemViewItem) which
    results in wrong paintings when e.g. a QTreeWidgetItem has a custom
    background color.
    Fix it by calling proxy()->drawPrimitive(PE_PanelItemViewItem) like it's
    done in qcommonstyle for the other styles.
    
    Merge conflict resolution for 6.8:
    - Resolved usage of QPainterStateGuard with QPainter::save and
      QPainter::restore
    
    Fixes: QTBUG-136130
    Change-Id: Ifd88b8b573c1c29ccaccd58329dcf35f76a1dc19
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit c0d8ed882fe09dd0e52a5847b45a6e2fc3a7fb87)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 44d0759552127da874093914fa76dd83514f39d5)
    (cherry picked from commit 17f86912bf3db2a5e8248007da6f4a4ae73e7d77)
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- src/plugins/styles/modernwindows/qwindows11style.cpp
+++ src/plugins/styles/modernwindows/qwindows11style.cpp
@@ -999,6 +999,16 @@ void QWindows11Style::drawPrimitive(PrimitiveElement element, const QStyleOption
         }
         break;
     }
+    case PE_PanelItemViewItem:
+        if (const QStyleOptionViewItem *vopt = qstyleoption_cast<const QStyleOptionViewItem *>(option)) {
+            if (vopt->backgroundBrush.style() != Qt::NoBrush) {
+                painter->save();
+                painter->setBrushOrigin(vopt->rect.topLeft());
+                painter->fillRect(vopt->rect, vopt->backgroundBrush);
+                painter->restore();
+            }
+        }
+        break;
     case PE_PanelItemViewRow:
         if (const QStyleOptionViewItem *vopt = qstyleoption_cast<const QStyleOptionViewItem *>(option)) {
             const QRect &rect = vopt->rect;
@@ -1667,6 +1677,9 @@ void QWindows11Style::drawControl(ControlElement element, const QStyleOption *op
                 QRect checkRect = proxy()->subElementRect(SE_ItemViewItemCheckIndicator, vopt, widget);
                 QRect iconRect = proxy()->subElementRect(SE_ItemViewItemDecoration, vopt, widget);
                 QRect textRect = proxy()->subElementRect(SE_ItemViewItemText, vopt, widget);
+
+                // draw the background
+                proxy()->drawPrimitive(PE_PanelItemViewItem, option, painter, widget);
 
                 const QRect &rect = vopt->rect;
                 const bool isRtl = option->direction == Qt::RightToLeft;
