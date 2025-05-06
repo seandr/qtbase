a436575b43dd75ad78a41dd9d2bfbb402ca84391
Matthias Rauter <matthias.rauter@qt.io>
2025-05-06 15:42:19 +0200

    Mark src/corelib/animation as security:significant
    Fixes: QTBUG-135185
    Change-Id: I6a3b43aa0b22278eb51d5ba4f31e0186e0930968
    Reviewed-by: Mate Barany <mate.barany@qt.io>
    (cherry picked from commit 11f2d3c86f41f0f05de0a74cf16166c4487a72a2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit ddf2a35f557a471a6d3c634e427e5934ec317493)
    (cherry picked from commit 0b434c8b4992a9e88712ff5b9aa5b83ea9ef71ab)

--- src/corelib/animation/qabstractanimation_p.h
+++ src/corelib/animation/qabstractanimation_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:significant reason:default
 
 #ifndef QABSTRACTANIMATION_P_H
 #define QABSTRACTANIMATION_P_H
