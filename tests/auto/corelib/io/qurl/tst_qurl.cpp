702051c07d9ea2baa9457afb151719dfd24eaa44
Thiago Macieira <thiago.macieira@intel.com>
2025-05-21 11:40:26 -0700

    QUrl: fix comparisons of URLs with password but no explicit username
    When a URL like "http://:pass@example.com" is parsed, we will set the
    username field to "present", but if you parse "http://example.com" and
    then set the password, the field would still be marked as absent. This
    commit fixes that, while restoring its absence if the password is later
    removed.
    
    [ChangeLog][QtCore][QUrl] Fixed a number of bugs in QUrl where a URL
    modified using the setXxx() functions would fail to compare equal to
    itself after going through toString() and setUrl() round-trip.
    
    Task-number: QTBUG-134900
    Task-number: QTBUG-134896
    Change-Id: I490cfd3d01260823ffdffffd4d9ac92dd42723b0
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    (cherry picked from commit 238a4eb170df9efb022440d88c9c366cc07b8ce6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8bc8f26e3c3b4e9960b2cf52dfe59e1fb6cdeca2)
    (cherry picked from commit 8d08bc24130ca5163de95be620a0aaee5ea677d5)
    (cherry picked from commit 97a206400ebd9f788c1c0a11bbee5a19182e14a1)

--- tests/auto/corelib/io/qurl/tst_qurl.cpp
+++ tests/auto/corelib/io/qurl/tst_qurl.cpp
@@ -1307,6 +1307,12 @@ void tst_QUrl::toString_constructed()
     QCOMPARE(url.toString(formattingOptions), asString);
     QCOMPARE(QString::fromLatin1(url.toEncoded(formattingOptions)), QString::fromLatin1(asEncoded)); // readable in case of differences
     QCOMPARE(url.toEncoded(formattingOptions), asEncoded);
+
+    if (options == QUrl::UrlFormattingOption::None) {
+        QUrl parsed(asString);
+        QCOMPARE(url, parsed);
+        QCOMPARE(qHash(url), qHash(parsed));
+    }
 }
 
 void tst_QUrl::toDisplayString_PreferLocalFile_data()
@@ -4141,6 +4147,11 @@ void tst_QUrl::setComponents()
         QCOMPARE(copy.toString(), toString);
         // Check round-tripping
         QCOMPARE(QUrl(copy.toString()).toString(), toString);
+
+        // check comparisons
+        QUrl recreated(toString);
+        QCOMPARE(copy, recreated);
+        QCOMPARE(qHash(copy), qHash(recreated));
     } else {
         QVERIFY(copy.toString().isEmpty());
     }
