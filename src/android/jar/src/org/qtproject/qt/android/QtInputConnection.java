591da0c4fa788989dd30e9ba6192e6efc7a6b6eb
Bartlomiej Moskal <bartlomiej.moskal@qt.io>
2025-06-03 13:00:26 +0200

    Android: Avoid restarting InputMethodManager during IME batch edits
    beginBatchEdit() means the start of a batch of related input operations
    from the Input Method Editor. We should not restart input until
    endBatchEdit() is called. Restarting it before may cause issue with
    inactive InputConnection.
    
    This commit fixes an issue where calling
    InputMethodManager.restartInput() during a batch edit (e.g., between
    beginBatchEdit and endBatchEdit).
    
    Fixes: QTBUG-136229
    Change-Id: I408e6dfd8a91f2d0f1dd8c18dc0dcc2d13cc3e38
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit a4850d0e0f42229afe2af10cee5794d0de70416c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit ddd53192d348d2c2751ca3b492bead42619f8200)
    (cherry picked from commit 9b85022d56f32af7922a0538415ffb325438f749)

--- src/android/jar/src/org/qtproject/qt/android/QtInputConnection.java
+++ src/android/jar/src/org/qtproject/qt/android/QtInputConnection.java
@@ -70,6 +70,7 @@ class QtInputConnection extends BaseInputConnection
 
     private static final String QtTAG = "QtInputConnection";
 
+    private boolean m_duringBatchEdit = false;
     private final QtInputConnectionListener m_qtInputConnectionListener;
 
     class HideKeyboardRunnable implements Runnable {
@@ -128,7 +129,7 @@ class QtInputConnection extends BaseInputConnection
 
     void restartImmInput()
     {
-        if (QtNativeInputConnection.fullscreenMode()) {
+        if (QtNativeInputConnection.fullscreenMode() && !m_duringBatchEdit) {
             if (m_imm != null)
                 m_imm.restartInput(m_view);
         }
@@ -139,6 +140,7 @@ class QtInputConnection extends BaseInputConnection
     public boolean beginBatchEdit()
     {
         setClosing(false);
+        m_duringBatchEdit = true;
         return QtNativeInputConnection.beginBatchEdit();
     }
 
@@ -158,7 +160,12 @@ class QtInputConnection extends BaseInputConnection
     public boolean endBatchEdit()
     {
         setClosing(false);
-        return QtNativeInputConnection.endBatchEdit();
+        boolean ret = QtNativeInputConnection.endBatchEdit();
+        if (m_duringBatchEdit) {
+            m_duringBatchEdit = false;
+            restartImmInput();
+        }
+        return ret;
     }
 
     @Override
