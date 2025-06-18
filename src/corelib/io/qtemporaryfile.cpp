1ced9132914c74a53339f135e9044132c2e621fb
Matthias Rauter <matthias.rauter@qt.io>
2025-06-18 13:58:33 +0200

    Mark qtemporaryfile.cpp as security-critical
    QTemporaryFileName parses and inspects a template filename, potentially
    from an untrusted source (althrough I would not recommend doing that).
    Therefore the file is marked security-critical.
    
    It might be a good idea to put QTemporaryFileName in it's own file to
    just mark that as security-critical.
    
    QUIP: 23
    Task-number: QTBUG-135187
    Change-Id: I6a6b720279330f450181e768c5d1a4d29be06387
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 18c57298e2af8fd20791208ab395e946ad3f9289)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 58976fbd8e844436704615495c37d000f6b84e76)
    (cherry picked from commit d6d0c45dd120cdc16dfac154c122621fa5cb911c)
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- src/corelib/io/qtemporaryfile.cpp
+++ src/corelib/io/qtemporaryfile.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2017 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:critical reason:data-parser
 
 #include "qtemporaryfile.h"
 
