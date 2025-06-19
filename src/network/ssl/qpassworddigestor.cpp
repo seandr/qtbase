37bd81040b630df0d6dd210f1759318f3e2c7cf9
Mårten Nordheim <marten.nordheim@qt.io>
2025-06-19 12:57:12 +0200

    CRA[network/ssl]: Mark qpassworddigestor critical
    Since it implements PBKDF1 and 2.
    
    Task-number: QTBUG-135586
    Pick-to: 6.9 6.8
    Change-Id: I620918473467ded58142efbc1b595c45283f4bef
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 8d6ffc4b529fcd402eafa55770d144dc34f4e2df)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b459df1d69d2f6203eda230d5bad893bbca38ed0)

--- src/network/ssl/qpassworddigestor.cpp
+++ src/network/ssl/qpassworddigestor.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:critical reason:cryptography
 
 #include "qpassworddigestor.h"
 
