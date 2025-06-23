2346663154a07ae804146b8110a140cc1c21ac4d
Jacek Poplawski <jacek.poplawski@qt.io>
2025-06-23 10:58:40 +0200

    Skip SO_BROADCAST for IPv6 sockets
    Ensure that BroadcastSocketOption is only set for IPv4 UDP sockets,
    as IPv6 does not support broadcasting. According to RFC 4291,
    broadcasting is replaced by multicasting in IPv6.
    
    Pick-to: 6.5
    Task-number: QTBUG-130070
    Change-Id: Ia3a98d276f659e8f2676743404370b8a15a0f179
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 28639f33f3b18a8ef7a7f7ca3db5fcaf8990fd21)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 78ff265b1a4e7b12080dbbae6f16419283ffaf27)
    (cherry picked from commit 55d58e1d5a936ef3daa970665423a50a4f80118b)

--- src/network/socket/qnativesocketengine.cpp
+++ src/network/socket/qnativesocketengine.cpp
@@ -430,11 +430,14 @@ bool QNativeSocketEngine::initialize(QAbstractSocket::SocketType socketType, QAb
 
     if (socketType == QAbstractSocket::UdpSocket) {
         // Set the broadcasting flag if it's a UDP socket.
-        if (!setOption(BroadcastSocketOption, 1)) {
-            d->setError(QAbstractSocket::UnsupportedSocketOperationError,
-                        QNativeSocketEnginePrivate::BroadcastingInitFailedErrorString);
-            close();
-            return false;
+        // IPv6 does not support broadcast — only set option for IPv4
+        if (protocol == QAbstractSocket::IPv4Protocol) {
+            if (!setOption(BroadcastSocketOption, 1)) {
+                d->setError(QAbstractSocket::UnsupportedSocketOperationError,
+                            QNativeSocketEnginePrivate::BroadcastingInitFailedErrorString);
+                close();
+                return false;
+            }
         }
 
         // Set some extra flags that are interesting to us, but accept failure
