5042e6b3ade19650cd88f10b9f81b70cc137dfc3
Kai Köhne <kai.koehne@qt.io>
2025-07-01 16:36:07 +0200

    qt_generate_deploy_app_script: Handle executable names with spaces
    Pick-to: 6.5
    Task-number: QTBUG-138155
    Change-Id: Ied2e3ee647f1bc6e7479d59e1f8a7316aaf302b2
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 078f06802a9b70027665dcb93adac5c6fad71e61)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8d1e9ffeaa233b02b99605eb01436b358e2be4a1)
    (cherry picked from commit fcbd905d3312a39feddd79b2b489706d87e93979)

--- src/corelib/Qt6CoreMacros.cmake
+++ src/corelib/Qt6CoreMacros.cmake
@@ -3842,7 +3842,7 @@ function(qt6_generate_deploy_app_script)
         qt6_generate_deploy_script(${generate_args}
             CONTENT "
 qt6_deploy_runtime_dependencies(
-    EXECUTABLE $<TARGET_FILE_NAME:${arg_TARGET}>.app
+    EXECUTABLE \"$<TARGET_FILE_NAME:${arg_TARGET}>.app\"
 ${common_deploy_args})
 ")
 
@@ -3850,7 +3850,7 @@ ${common_deploy_args})
         qt6_generate_deploy_script(${generate_args}
             CONTENT "
 qt6_deploy_runtime_dependencies(
-    EXECUTABLE $<TARGET_FILE:${arg_TARGET}>
+    EXECUTABLE \"$<TARGET_FILE:${arg_TARGET}>\"
     GENERATE_QT_CONF
 ${common_deploy_args})
 ")
@@ -3860,7 +3860,7 @@ ${common_deploy_args})
         qt6_generate_deploy_script(${generate_args}
             CONTENT "
 qt6_deploy_runtime_dependencies(
-    EXECUTABLE $<TARGET_FILE:${arg_TARGET}>
+    EXECUTABLE \"$<TARGET_FILE:${arg_TARGET}>\"
     GENERATE_QT_CONF
 ${common_deploy_args})
 ")
