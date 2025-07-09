1f8387c1920b72d9c87fd6783328db807bb8353f
Thiago Macieira <thiago.macieira@intel.com>
2025-07-09 11:43:56 -0700

    Revert renaming of QArrayData::ref_
    This reverts part of commit 3f61f736266ece40d627dcf6214618a22a009fd1.
    The renaming of the member caused Qt Creator to break. Since that was a
    gratuitous change and was backported, it's hard to fix.
    
    src/libs/utils/stringtable.cpp:97:44: error: ‘QArrayDataPointer<char16_t>::Data’ {aka ‘struct QTypedArrayData<char16_t>’} has no member named ‘ref_’; did you mean ‘ref’?
    
    Change-Id: I5d0b1a0c9b42adc38c7bfffd1a86e7abb08d952b
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    (cherry picked from commit 858ac29d6747d9013b9e5aeb056b54c47e980329)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit eee082109677bea71105bc7c9dd15ad0ecf41f87)
    (cherry picked from commit 5ae7ea693690e84e48c85e65cfb798f340fd2d0a)

--- src/corelib/tools/qarraydata.h
+++ src/corelib/tools/qarraydata.h
@@ -32,7 +32,7 @@ struct QArrayData
     };
     Q_DECLARE_FLAGS(ArrayOptions, ArrayOption)
 
-    QBasicAtomicInt m_ref;
+    QBasicAtomicInt ref_;
     ArrayOptions flags;
     qsizetype alloc;
 
@@ -49,19 +49,19 @@ struct QArrayData
     /// Returns true if sharing took place
     bool ref() noexcept
     {
-        m_ref.ref();
+        ref_.ref();
         return true;
     }
 
     /// Returns false if deallocation is necessary
     bool deref() noexcept
     {
-        return m_ref.deref();
+        return ref_.deref();
     }
 
     bool isShared() const noexcept
     {
-        return m_ref.loadRelaxed() != 1;
+        return ref_.loadRelaxed() != 1;
     }
 
     // Returns true if a detach is necessary before modifying the data
@@ -69,7 +69,7 @@ struct QArrayData
     // detaching is necessary, you should be in a non-const function already
     bool needsDetach() const noexcept
     {
-        return m_ref.loadRelaxed() > 1;
+        return ref_.loadRelaxed() > 1;
     }
 
     qsizetype detachCapacity(qsizetype newSize) const noexcept
