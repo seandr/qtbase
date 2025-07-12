a8d5f83912137eae48ba56f7357509c27b2764d8
Assam Boudjelthia <assam.boudjelthia@qt.io>
2025-07-12 18:00:30 +0300

    CRA: rectify androidcontentfileengine.cpp and extract.cpp files
    Under androidcontentfileengine.cpp, mainly parsing and
    encoding/decoding of file paths are done, so a more
    accurate reason is data-parser instead of file-handling.
    
    Under extract.cpp, the criticality is not very accurate
    either, as the only way we're using this code is to extract
    Android system style, and it's not expected to have security
    concerns or be affected by a 3rd party.
    
    Amends a6caa394ba49cb58cc07613f9a5fc6bfb5975e3b.
    
    Task-number: QTBUG-136818
    Task-number: QTBUG-135178
    Task-number: QTBUG-136816
    Change-Id: Id582d92befb000afbe4d9b01f79ef220e274711e
    Reviewed-by: Dimitrios Apostolou <jimis@qt.io>
    (cherry picked from commit 8c626ad667ef50a610acc04403a908fbd068a654)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5b11dc0a3ac7b3f41df1807a484241afd0caf7fa)
    (cherry picked from commit dd60a6b27774769ef6fe82cacc33f0995323bf9a)

--- src/plugins/platforms/android/androidcontentfileengine.cpp
+++ src/plugins/platforms/android/androidcontentfileengine.cpp
@@ -1,7 +1,7 @@
 ﻿// Copyright (C) 2019 Volker Krause <vkrause@kde.org>
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
-// Qt-Security score:critical reason:file-handling
+// Qt-Security score:critical reason:data-parser
 
 #include "androidcontentfileengine.h"
 
--- src/plugins/platforms/android/extract.cpp
+++ src/plugins/platforms/android/extract.cpp
@@ -1,7 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2014 BogDan Vatra <bogdan@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
-// Qt-Security score:critical reason:data-serializing
 
 #include <QtCore/QJniEnvironment>
 
