f661e21a71070e81cedbd07024e7ceba09e62107
Marc Mutz <marc.mutz@qt.io>
2025-07-18 11:01:47 +0200

    QTextStream: prefer QStringView overload of Private::putString()
    This call is safer than the (ptr, n, bool=false) one, which may
    accidentally resolve to (view, bool) if decltype(ptr) is "wrong".
    
    This is in preparation of removing said putString() overload (and the
    bool parameter).
    
    Manual conflict resolution for 6.9:
    - simple line conflict due to b9faf83fd66cb776e8b8c1f62a21ba56f60beff7
      not being picked back
    
    Task-number: QTBUG-138520
    Pick-to: 6.5
    Change-Id: I93ff89322468558f7da723ae47a1d67852cc59e6
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 897567c4eef02be6e22e6f15df66a473dca966c5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 821270975bf31f77c1975829a09b748a070958ff)
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit f6a0909bbb3634d87a7a2901a1bce2ad296e154e)

--- src/corelib/serialization/qtextstream.cpp
+++ src/corelib/serialization/qtextstream.cpp
@@ -786,7 +786,7 @@ inline void QTextStreamPrivate::ungetChar(QChar ch)
 inline void QTextStreamPrivate::putChar(QChar ch)
 {
     if (params.fieldWidth > 0)
-        putString(&ch, 1);
+        putString(QStringView{&ch, 1});
     else
         write(ch);
 }
@@ -2478,7 +2478,7 @@ QTextStream &QTextStream::operator<<(QStringView string)
 {
     Q_D(QTextStream);
     CHECK_VALID_STREAM(*this);
-    d->putString(string.cbegin(), int(string.size()));
+    d->putString(string);
     return *this;
 }
 
