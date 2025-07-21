ef6fec0835e0ded379a7b2e0992b5a7705c27c25
Marc Mutz <marc.mutz@qt.io>
2025-07-21 09:27:56 +0200

    Mark qlocale_<os>.cpp's as security-critical
    They all parse data in one form or another, and so should be marked
    the same way qlocale.cpp already is.
    
    The ICU backend, e.g. has to deal with the fact that ICU only has
    32-bit sizes (and it doesn't :/).
    
    Amends 8df072fc8006510c9b743e8ffedaaf51a876883a.
    
    Manual conflict resolution for 6.8:
    - simple, but annoying, line conflict due to Legal changing the SPDX
      identifiers in LTS branches
    
    Task-number: QTBUG-135195
    Change-Id: I0741c81670c6eb69a58f79567a437bad70398839
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit f1c1f8575a97a7d7758d1a1112f7aa912f85e11b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit da47d8d930aeee5b629cc1738b97ae4e45215bff)
    (cherry picked from commit bce0f0b5b316552c16980960f64c2d4334262e60)
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- src/corelib/text/qlocale_win.cpp
+++ src/corelib/text/qlocale_win.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2016 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:critical reason:data-parser
 
 #include "qlocale_p.h"
 #include "qlocale_tools_p.h"
