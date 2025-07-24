9b8e9c3738b5dbac554db34e0585a1e0be882b8a
Jan Moeller <Jan.Moeller@governikus.de>
2025-07-24 12:47:25 +0200

    a11y Android: Forward new QAccessibleAnnouncementEvent on Android
    Android support was missing and the QAccessibleAnnouncementEvent had
    no effect, this implements its support on Android.
    
    Task-number: QTBUG-75003
    Task-number: QTBUG-135413
    Change-Id: Ic51920f7181c84ee54c729cb50b9b1418e208afd
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit a4cc955cb916a9a079fabecfe810bec9635b076b)
    (cherry picked from commit 590637791e50305dc52869a4c469f212b63ec3d4)
    (cherry picked from commit 05c6d393c70d6487de8a1bf3732a82d42b82eb29)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/android/jar/src/org/qtproject/qt/android/QtActivityDelegate.java
+++ src/android/jar/src/org/qtproject/qt/android/QtActivityDelegate.java
@@ -293,6 +293,12 @@ class QtActivityDelegate extends QtActivityDelegateBase
         });
     }
 
+    @Override
+    public void notifyAnnouncementEvent(int viewId, String message)
+    {
+        m_accessibilityDelegate.notifyAnnouncementEvent(viewId, message);
+    }
+
     // QtMenuInterface implementation begin
     @Override
     public void resetOptionsMenu()
