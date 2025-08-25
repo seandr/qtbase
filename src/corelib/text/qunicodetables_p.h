c8f56f2a77e659ed0255c267ecda672ff957671c
Marc Mutz <marc.mutz@qt.io>
2025-08-25 13:52:30 +0200

    QUnicodeTables: abstract access to Properties::cases
    This is in preparation for storing this information in a separate
    array to save space by removing the many duplicates in there.
    
    Pick to all active branches to have the same internal API going
    forward, even if we don't pick the storage optimization back as far.
    
    Qt 6.5 doesn't have QSpan, yet (not even as private API), but
    returning a reference to const std::array<.,4> will be an adequate
    replacement. To enable that without casting, convert `cases` from a C
    array to a std::array. For some reason, this requires extra
    parentheses, so add them.
    
    Manual conflict resolution for 6.8:
    - simple line conflcit due to the continued presence of the
      properties(char16_t) overload in this branch
    - re-ran util/unicode/unicode to regenerate the tables in this
      branch's format
    
    Task-number: QTBUG-139427
    Pick-to: 6.5
    Change-Id: I5331fd6d71a6a447b0445d8235b5eb8e38178e2e
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit eaf77edebdf1c0b1682c28c014f2d81479af0051)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5c6e78a2fe9d5033212f82aee0915519a70ace2b)
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- src/corelib/text/qunicodetables_p.h
+++ src/corelib/text/qunicodetables_p.h
@@ -21,6 +21,10 @@
 
 #include <QtCore/qchar.h>
 
+#include <QtCore/qspan.h>
+
+#include <array>
+
 QT_BEGIN_NAMESPACE
 
 #define UNICODE_DATA_VERSION QChar::Unicode_16_0
@@ -36,6 +40,11 @@ enum Case {
     NumCases
 };
 
+struct CaseConversion {
+    ushort special    : 1;
+    signed short diff : 15;
+};
+
 struct Properties {
     ushort category            : 8; /* 5 used */
     ushort direction           : 8; /* 5 used */
@@ -49,10 +58,7 @@ struct Properties {
 #ifdef Q_OS_WASM
     unsigned char              : 0; //wasm 64 packing trick
 #endif
-    struct {
-        ushort special    : 1;
-        signed short diff : 15;
-    } cases[NumCases];
+    std::array<CaseConversion, NumCases> cases;
 #ifdef Q_OS_WASM
     unsigned char              : 0; //wasm 64 packing trick
 #endif
@@ -68,6 +74,9 @@ Q_DECL_CONST_FUNCTION
 Q_CORE_EXPORT const Properties * QT_FASTCALL properties(char32_t ucs4) noexcept;
 Q_DECL_CONST_FUNCTION
 Q_CORE_EXPORT const Properties * QT_FASTCALL properties(char16_t ucs2) noexcept;
+
+Q_DECL_CONST_FUNCTION Q_CORE_EXPORT
+QSpan<const CaseConversion, NumCases> QT_FASTCALL caseConversion(char32_t ucs4) noexcept;
 
 static_assert(sizeof(Properties) == 20);
 
