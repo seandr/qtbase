66622f8434669beee59ae0b6569f454bbef94c62
Assam Boudjelthia <assam.boudjelthia@qt.io>
2025-09-16 17:43:20 +0300

    Android: change only the opacity of system bars when setting system ui
    When switching between expanded mode (similar to edge-to-edge) and
    back to normal mode, we shouldn't be touching the colors of the
    system bars, as that can mess up with the theme values or user
    defined values. Instead, simply change the opacity (alpha bits)
    of the color so we get transparent bars in expanded mode and
    opaque bars in normal mode.
    
    Task-number: QTBUG-139690
    Change-Id: Iadc5a9d8625c50009a0dc1a5ac29b5354089d345
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit d59bd61d8e7aa11598853a422498239b6a1d6b6f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 549a536d6d87f142dd169c45adec01e84943ea38)

--- src/android/jar/src/org/qtproject/qt/android/QtDisplayManager.java
+++ src/android/jar/src/org/qtproject/qt/android/QtDisplayManager.java
@@ -6,6 +6,7 @@ package org.qtproject.qt.android;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
 import android.os.Build;
@@ -186,25 +187,59 @@ class QtDisplayManager
             decorView.setSystemUiVisibility(systemUiVisibility);
         }
 
-        // Handle transparent status and navigation bars
-        if (m_expandedToCutout) {
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.setNavigationBarColor(Color.TRANSPARENT);
-        } else {
-            // Restore theme's system bars colors
-            Theme theme = m_activity.getTheme();
-            TypedValue typedValue = new TypedValue();
+        if (!isFullScreen && !edgeToEdgeEnabled(m_activity)) {
+            // These are needed to operate on system bar colors
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
-            theme.resolveAttribute(android.R.attr.statusBarColor, typedValue, true);
-            int defaultStatusBarColor = typedValue.data;
-            window.setStatusBarColor(defaultStatusBarColor);
+            // Handle transparent status and navigation bars
+            if (m_expandedToCutout) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    window.setStatusBarColor(Color.TRANSPARENT);
+                    window.setNavigationBarColor(Color.TRANSPARENT);
+                } else {
+                    // Android 9 and prior doesn't add the semi-transparent bars
+                    // to avoid low contrast system icons, so try to mimick it
+                    // by taking the current color and only increase the opacity.
+                    int statusBarColor = window.getStatusBarColor();
+                    int transparentStatusBar = statusBarColor & 0x00FFFFFF;
+                    window.setStatusBarColor(transparentStatusBar);
 
-            theme.resolveAttribute(android.R.attr.navigationBarColor, typedValue, true);
-            int defaultNavigationBarColor = typedValue.data;
-            window.setNavigationBarColor(defaultNavigationBarColor);
+                    int navigationBarColor = window.getNavigationBarColor();
+                    int semiTransparentNavigationBar = navigationBarColor & 0x7FFFFFFF;
+                    window.setNavigationBarColor(semiTransparentNavigationBar);
+                }
+            } else {
+                // Restore theme's system bars colors
+                Theme theme = m_activity.getTheme();
+                TypedValue typedValue = new TypedValue();
+
+                theme.resolveAttribute(android.R.attr.statusBarColor, typedValue, true);
+                int defaultStatusBarColor = typedValue.data;
+                window.setStatusBarColor(defaultStatusBarColor);
+
+                theme.resolveAttribute(android.R.attr.navigationBarColor, typedValue, true);
+                int defaultNavigationBarColor = typedValue.data;
+                window.setNavigationBarColor(defaultNavigationBarColor);
+            }
         }
 
         decorView.post(() -> decorView.requestApplyInsets());
+    }
+
+    private static boolean edgeToEdgeEnabled(Activity activity) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.VANILLA_ICE_CREAM)
+            return true;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM)
+            return false;
+        int[] attrs = new int[] { android.R.attr.windowOptOutEdgeToEdgeEnforcement };
+        TypedArray ta = activity.getTheme().obtainStyledAttributes(attrs);
+        try {
+            return !ta.getBoolean(0, false);
+        } finally {
+            ta.recycle();
+        }
     }
 
     boolean isFullScreen()
