971b1b8cfb307230b72726cc7f31b96a03ee4425
Piotr Wiercinski <piotr.wiercinski@qt.io>
2025-09-17 10:17:48 +0200

    Assess security level for files under qtbase/src/widgets/dialogs
    Assign the default level to all files but qfiledialog.cpp which parses
    input in restoreState().
    
    Fixes: QTBUG-135735
    Change-Id: I27e1f18e26e070130018ccf6fa47ccf78b635dc8
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 70d572aba304ed369b3463453ff3a30ad5b5c030)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 9761888f50f3cc4b3af8c05628f40e20d73a282a)
    (cherry picked from commit f2f456cc0df30b58f98e82880d80620d0aef2214)
    Reviewed-by: Piotr Wierciński <piotr.wiercinski@qt.io>

--- src/widgets/dialogs/qdialog.cpp
+++ src/widgets/dialogs/qdialog.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:significant reason:default
 
 #include <QtWidgets/qtwidgetsglobal.h>
 #if QT_CONFIG(colordialog)
--- src/widgets/dialogs/qfscompleter_p.h
+++ src/widgets/dialogs/qfscompleter_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:significant reason:default
 
 #ifndef QFSCOMPLETOR_P_H
 #define QFSCOMPLETOR_P_H
