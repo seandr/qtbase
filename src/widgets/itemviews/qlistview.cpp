54a127769fa0ed57723e41b86a0dd8ea7e2fb430
Piotr Wiercinski <piotr.wiercinski@qt.io>
2025-09-17 16:37:08 +0200

    Assess security level for files under qtbase/src/widgets/itemviews
    Assign the default level to all files but qheaderview.cpp which parses
    input in read().
    
    QUIP: 23
    Fixes: QTBUG-135739
    Change-Id: Idf295f0af63b418793a67d43c1c1d6e39dc92d37
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 824c333aac9265fe457658d77c36b6f600fcf8e5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 631cb8ecf4a87b9218fa474823da9d0746484968)
    Reviewed-by: Piotr Wierciński <piotr.wiercinski@qt.io>

--- src/widgets/itemviews/qlistview.cpp
+++ src/widgets/itemviews/qlistview.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2013 Samuel Gaist <samuel.gaist@deltech.ch>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:significant reason:default
 
 #include "qlistview.h"
 
--- src/widgets/itemviews/qtreewidgetitemiterator.cpp
+++ src/widgets/itemviews/qtreewidgetitemiterator.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// Qt-Security score:significant reason:default
 
 #include <private/qtreewidgetitemiterator_p.h>
 #include "qtreewidget.h"
