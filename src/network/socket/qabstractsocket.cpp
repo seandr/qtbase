bc9b0e70cccda19a0f44db53144c1b9cae8e7720
Ivan Solovev <ivan.solovev@qt.io>
2025-09-22 11:47:14 +0200

    Allow binding a socket to a specific QNetworkInterface
    For now, via a private API.
    
    Provide a new socket engine option - BindInterfaceIndex, and use it if
    a valid interface is passed to QAbstractSocketPrivate::bind().
    
    Note that this feature is not fully functional on macOS:
    both SO_BINDTODEVICE and IP{V6}_BOUND_IF only work for limiting
    outgoing datagrams. The patch uses IP{V6}_BOUND_IF, because it allows
    to avoid number -> string -> number conversion.
    As of now, extra filtering of incoming data should be done on the user
    side.
    
    Task-number: QTBUG-80704
    Task-number: QTBUG-139697
    Change-Id: Ic207908313d9d25f96c23ecc363181ff0ae8232a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 83fc4ff9c20eb922113e08ba8566e077ce44dea3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit fad781c2658c388746f625cfca3345f906f475f4)

--- src/network/socket/qabstractsocket.cpp
+++ src/network/socket/qabstractsocket.cpp
@@ -432,6 +432,7 @@
 
 #include "qabstractsocket.h"
 #include "qabstractsocket_p.h"
+#include "qnetworkinterface.h"
 
 #include "private/qhostinfo_p.h"
 
@@ -1504,8 +1505,6 @@ bool QAbstractSocketPrivate::bind(const QHostAddress &address, quint16 port, QAb
 {
     Q_Q(QAbstractSocket);
 
-    Q_UNUSED(iface); // will be used in a follow-up patch
-
     // now check if the socket engine is initialized and to the right type
     if (!socketEngine || !socketEngine->isValid()) {
         QHostAddress nullAddress;
@@ -1537,6 +1536,10 @@ bool QAbstractSocketPrivate::bind(const QHostAddress &address, quint16 port, QAb
         socketEngine->setOption(QAbstractSocketEngine::BindExclusively, 0);
 #endif
     }
+#if QT_CONFIG(networkinterface)
+    if (iface && iface->isValid())
+        socketEngine->setOption(QAbstractSocketEngine::BindInterfaceIndex, iface->index());
+#endif
     bool result = socketEngine->bind(address, port);
     cachedSocketDescriptor = socketEngine->socketDescriptor();
 
