66fe51c62dec57af1a9328f49db78dd5dd353ae8
Igor Khanin <igor@khanin.biz>
2025-09-24 22:08:37 +0300

    QMacStyle: Draw disclosure triangles according to correct direction
    When using a QTreeView and overriding its' (or any ancestor widget's)
    layout direction from the default, on the Mac style the indicator
    branches would be rendered pointing the wrong way - as those are drawn
    as native disclosure triangle buttons whose NSCell did not get the
    proper layout direction passed on to. This fixes it.
    
    Change-Id: Ic708e82dd2534af674822fa2432ce4a4af22c522
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 4caaa435ca1ee94e7042b8ae6f7023035b977c5c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e935d92fc39b53867cf8810e1890932ae4a8d81f)

--- src/plugins/styles/mac/qmacstyle_mac.mm
+++ src/plugins/styles/mac/qmacstyle_mac.mm
@@ -800,6 +800,18 @@ static bool qt_macWindowMainWindow(const QWidget *window)
     return false;
 }
 
+static NSUserInterfaceLayoutDirection qt_macLayoutDirectionFromQt(Qt::LayoutDirection direction)
+{
+    switch (direction) {
+    case Qt::LeftToRight:
+        return NSUserInterfaceLayoutDirectionLeftToRight;
+    case Qt::RightToLeft:
+        return NSUserInterfaceLayoutDirectionRightToLeft;
+    case Qt::LayoutDirectionAuto:
+        return [NSApp userInterfaceLayoutDirection];
+    }
+}
+
 /*****************************************************************************
   QMacCGStyle globals
  *****************************************************************************/
@@ -3451,6 +3463,7 @@ void QMacStyle::drawPrimitive(PrimitiveElement pe, const QStyleOption *opt, QPai
         [triangleCell setState:(opt->state & State_Open) ? NSControlStateValueOn : NSControlStateValueOff];
         bool viewHasFocus = (w && w->hasFocus()) || (opt->state & State_HasFocus);
         [triangleCell setBackgroundStyle:((opt->state & State_Selected) && viewHasFocus) ? NSBackgroundStyleEmphasized : NSBackgroundStyleNormal];
+        [triangleCell setUserInterfaceLayoutDirection:qt_macLayoutDirectionFromQt(opt->direction)];
 
         d->setupNSGraphicsContext(cg, NO);
 
