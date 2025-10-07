b37bac9f1498110438aaa29c6426147f4618dbd8
Ahmad Samir <a.samirh78@gmail.com>
2025-10-07 22:08:31 +0300

    tst_QArrayData: port away from QPair
    Pick-to: 6.5
    Task-number: QTBUG-115841
    Change-Id: Ia5d90782f12c27715cec3a76c100c3563ac82b0e
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit c580fcbdf835b9b54709a1bbc873805b4764425a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 32380f229d6c8fb6935f6c60db5af4f022df5381)

--- tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
+++ tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
@@ -586,7 +586,7 @@ void tst_QArrayData::typedData()
     {
         Deallocator keeper(sizeof(char),
                 alignof(QTypedArrayData<char>::AlignmentDummy));
-        QPair<QTypedArrayData<char> *, char *> pair = QTypedArrayData<char>::allocate(10);
+        std::pair<QTypedArrayData<char> *, char *> pair = QTypedArrayData<char>::allocate(10);
         QArrayData *array = pair.first;
         keeper.headers.append(array);
 
@@ -606,7 +606,7 @@ void tst_QArrayData::typedData()
     {
         Deallocator keeper(sizeof(short),
                 alignof(QTypedArrayData<short>::AlignmentDummy));
-        QPair<QTypedArrayData<short> *, short *> pair = QTypedArrayData<short>::allocate(10);
+        std::pair<QTypedArrayData<short> *, short *> pair = QTypedArrayData<short>::allocate(10);
         QArrayData *array = pair.first;
         keeper.headers.append(array);
 
@@ -626,7 +626,7 @@ void tst_QArrayData::typedData()
     {
         Deallocator keeper(sizeof(double),
                 alignof(QTypedArrayData<double>::AlignmentDummy));
-        QPair<QTypedArrayData<double> *, double *> pair = QTypedArrayData<double>::allocate(10);
+        std::pair<QTypedArrayData<double> *, double *> pair = QTypedArrayData<double>::allocate(10);
         QArrayData *array = pair.first;
         keeper.headers.append(array);
 
