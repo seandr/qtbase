8f8e2b3889ed0ad463d1a607c899a5f2dc17a78a
Marc Mutz <marc.mutz@qt.io>
2025-11-17 08:22:43 +0100

    QHash: fix noexcept on Node::takeValue()
    In the QHashDummyValue specialization, the noexcept was missing. Add
    it.
    
    In the primary Node template, takeValue() was conditionally noexcept,
    but seeing as it just returns an rvalue reference to the 'value' data
    member, it's of course unconditionally noexcept, since the actual move
    contructor or -assignment operator call happens after the function
    returns.
    
    Besides, even if it had to be conditionally noexcept (e.g., because it
    returned T-by-value), it would have to check for
    nothrow_move_constructible, not _assignable.
    
    Amends 5b7c3e31b538376f2b4733bd868b5875b504cdb3.
    
    Picking to older branches is ok, since these functions, even if they
    were marked noexcept(false), could not throw any exceptions, so
    forward BC is not broken.
    
    Pick-to: 6.5
    Change-Id: I43aab284acc8b1303d4147c1b8c58613bbb05b25
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 5d82ccc4e130de70492083fec0f61c6bd149e31b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8c5bdeccba1e6825798ed36d0c39b87c9ed262ae)

--- src/corelib/tools/qhash.h
+++ src/corelib/tools/qhash.h
@@ -86,7 +86,7 @@ struct Node
     {
         value = T(std::forward<Args>(args)...);
     }
-    T &&takeValue() noexcept(std::is_nothrow_move_assignable_v<T>)
+    T &&takeValue() noexcept
     {
         return std::move(value);
     }
@@ -109,7 +109,7 @@ struct Node<Key, QHashDummyValue> {
     void emplaceValue(Args &&...)
     {
     }
-    ValueType takeValue() { return QHashDummyValue(); }
+    ValueType takeValue() noexcept { return QHashDummyValue(); }
     bool valuesEqual(const Node *) const { return true; }
 };
 
