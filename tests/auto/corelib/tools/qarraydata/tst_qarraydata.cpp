seandr/qtbase

commit 09d4b16046edc104ba1646d7398daf0be3eb9eeb

Remove second parameter being passed to QSKIP()
Prior to Qt 5, QSKIP() accepted a scope parameter, either SkipAll or
SkipSingle. This has been ignored since 5.0.0, so remove all surviving
uses of it (including the QTest::QSkipAll and QTest::SkipAll, which
were surely bogus even before).

Change-Id: I4fc671534c88c2c52535d4cc9f1ab0bd8ea719d0
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
@@ -1076,7 +1076,7 @@ void tst_QArrayData::arrayOpsExtra_data()
 
 void tst_QArrayData::arrayOpsExtra()
 {
-    QSKIP("Skipped while changing QArrayData operations.", SkipAll);
+    QSKIP("Skipped while changing QArrayData operations.");
     QFETCH(QArrayData::GrowthPosition, GrowthPosition);
     CountedObject::LeakChecker leakChecker; Q_UNUSED(leakChecker);
 

tests/auto/corelib/io/qfile/tst_qfile.cpp
@@ -996,7 +996,7 @@ private:
 void tst_QFile::readAllStdin()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
 #if defined(Q_OS_ANDROID)
     QSKIP("This test crashes when doing nanosleep. See QTBUG-69034.");
@@ -1021,7 +1021,7 @@ void tst_QFile::readAllStdin()
 void tst_QFile::readLineStdin()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
 #if defined(Q_OS_ANDROID)
     QSKIP("This test crashes when doing nanosleep. See QTBUG-69034.");
@@ -1064,7 +1064,7 @@ void tst_QFile::readLineStdin()
 void tst_QFile::readLineStdin_lineByLine()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
 #if defined(Q_OS_ANDROID)
     QSKIP("This test crashes when calling ::poll. See QTBUG-69034.");

tests/auto/corelib/io/qfileinfo/tst_qfileinfo.cpp
@@ -1301,7 +1301,7 @@ void tst_QFileInfo::isSymLink_data()
 void tst_QFileInfo::isSymLink()
 {
 #ifdef Q_NO_SYMLINKS
-    QSKIP("No symlink support", SkipAll);
+    QSKIP("No symlink support");
 #else
     QFETCH(QString, path);
     QFETCH(bool, isSymLink);

tests/auto/corelib/io/qsettings/tst_qsettings.cpp
@@ -2363,7 +2363,7 @@ void tst_QSettings::testRegistryShortRootNames()
 void tst_QSettings::testRegistry32And64Bit()
 {
 #if !defined (Q_OS_WIN)
-    QSKIP("This test is specific to the Windows registry.", SkipAll);
+    QSKIP("This test is specific to the Windows registry.");
 #else
 
     const QString key("HKEY_LOCAL_MACHINE\\Software");

tests/auto/corelib/ipc/qsharedmemory/tst_qsharedmemory.cpp
@@ -529,13 +529,13 @@ void tst_QSharedMemory::emptyMemory()
 void tst_QSharedMemory::readOnly()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #elif defined(Q_OS_MACOS)
-    QSKIP("QTBUG-59936: Times out on macOS", SkipAll);
+    QSKIP("QTBUG-59936: Times out on macOS");
 #elif defined(Q_OS_WIN)
     QSKIP("This test opens a crash dialog on Windows.");
 #elif defined(__SANITIZE_ADDRESS__) || __has_feature(address_sanitizer)
-    QSKIP("ASan prevents the crash this test is looking for.", SkipAll);
+    QSKIP("ASan prevents the crash this test is looking for.");
 #else
     QNativeIpcKey key = rememberKey("readonly_segfault");
 
@@ -860,7 +860,7 @@ void tst_QSharedMemory::simpleProcessProducerConsumer_data()
 void tst_QSharedMemory::simpleProcessProducerConsumer()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     QFETCH(int, processes);
 

tests/auto/corelib/ipc/qsystemsemaphore/tst_qsystemsemaphore.cpp
@@ -232,7 +232,7 @@ void tst_QSystemSemaphore::twoSemaphores()
 void tst_QSystemSemaphore::basicProcesses()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     QNativeIpcKey key = platformSafeKey("store");
     QSystemSemaphore sem(key, 0, QSystemSemaphore::Create);
@@ -269,7 +269,7 @@ void tst_QSystemSemaphore::processes_data()
 void tst_QSystemSemaphore::processes()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     QNativeIpcKey key = platformSafeKey("store");
     QSystemSemaphore sem(key, 1, QSystemSemaphore::Create);
@@ -297,7 +297,7 @@ void tst_QSystemSemaphore::processes()
 void tst_QSystemSemaphore::undo()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     QNativeIpcKey key = platformSafeKey("store");
     switch (key.type()) {
@@ -331,7 +331,7 @@ void tst_QSystemSemaphore::undo()
 void tst_QSystemSemaphore::initialValue()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     QNativeIpcKey key = platformSafeKey("store");
     QSystemSemaphore sem(key, 1, QSystemSemaphore::Create);

tests/auto/corelib/kernel/qobject/tst_qobject.cpp
@@ -3079,7 +3079,7 @@ void tst_QObject::dynamicProperties()
 void tst_QObject::recursiveSignalEmission()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     QProcess proc;
     // signalbug helper app should always be next to this test binary

tests/auto/corelib/mimetypes/qmimedatabase/qmimedatabase-cache-builtin/tst_qmimedatabase-cache-builtin.cpp
@@ -6,7 +6,7 @@
 void tst_QMimeDatabase::initTestCaseInternal()
 {
 #if !QT_CONFIG(process)
-    QSKIP("No qprocess support", SkipAll);
+    QSKIP("No qprocess support");
 #else
     const QString mimeDirName = m_globalXdgDir + QStringLiteral("/mime");
     runUpdateMimeDatabase(mimeDirName);

commit b124663d57e2226756aa9b93ac781ba349536eba

QArrayDataOps: reactivate arrayOpsExtra test
Amends commit b99271caa6231ad753bc796dae5202ebc1cb9440, which added the
temporary QSKIP but forgot to remove.

Manual conflict resolution for 6.8:
- Simple line conflict caused by 09d4b16046edc104ba1646d7398daf0be3eb9eeb
  not having been picked back to relevant branches.

Pick-to: 6.5
Change-Id: I7d7a2658f607fe28d9a9fffd9ba5bd55fdc2f839
Reviewed-by: Marc Mutz <marc.mutz@qt.io>
Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
(cherry picked from commit bd5fc184bf9301aee49dfdaef689e581e84fcc9f)
Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
(cherry picked from commit 00a1c519307b5477812deb60bc330e52b084ec0a)
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
@@ -1076,7 +1076,6 @@ void tst_QArrayData::arrayOpsExtra_data()
 
 void tst_QArrayData::arrayOpsExtra()
 {
-    QSKIP("Skipped while changing QArrayData operations.", SkipAll);
     QFETCH(QArrayData::GrowthPosition, GrowthPosition);
     CountedObject::LeakChecker leakChecker; Q_UNUSED(leakChecker);
 
@@ -1102,9 +1101,18 @@ void tst_QArrayData::arrayOpsExtra()
         auto s = QArrayDataPointer<QString>::allocateGrow(QArrayDataPointer<QString>(), alloc, GrowthPosition);
         auto o = QArrayDataPointer<CountedObject>::allocateGrow(QArrayDataPointer<CountedObject>(), alloc, GrowthPosition);
         if (initialSize) {
-            i->appendInitialize(initialSize);
-            s->appendInitialize(initialSize);
-            o->appendInitialize(initialSize);
+            if (GrowthPosition == QArrayData::GrowsAtEnd) {
+                i->appendInitialize(initialSize);
+                s->appendInitialize(initialSize);
+                o->appendInitialize(initialSize);
+            } else {
+                // there's no prependInitialize()
+                for (qsizetype n = 0; n < initialSize; ++n) {
+                    i->emplace(0);
+                    s->emplace(0);
+                    o->emplace(0);
+                }
+            }
         }
 
         // assign unique values
@@ -1142,7 +1150,8 @@ void tst_QArrayData::arrayOpsExtra()
     }
 
     // copyAppend (iterator version)
-    {
+    // (appending requires growth at end)
+    if (GrowthPosition == QArrayData::GrowsAtEnd) {
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         const auto testCopyAppend = [&] (auto &dataPointer, auto first, auto last) {
             const size_t originalSize = dataPointer.size;
@@ -1191,7 +1200,8 @@ void tst_QArrayData::arrayOpsExtra()
     }
 
     // copyAppend (iterator version) - special case of copying from self iterators
-    {
+    // (appending requires growth at end)
+    if (GrowthPosition == QArrayData::GrowsAtEnd) {
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         const auto testCopyAppendSelf = [&] (auto &dataPointer, auto first, auto last) {
             const size_t originalSize = dataPointer.size;
@@ -1244,7 +1254,8 @@ void tst_QArrayData::arrayOpsExtra()
     }
 
     // copyAppend (value version)
-    {
+    // (appending requires growth at end)
+    if (GrowthPosition == QArrayData::GrowsAtEnd) {
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         const auto testCopyAppend = [&] (auto &dataPointer, size_t n, auto value) {
             const size_t originalSize = dataPointer.size;
@@ -1291,7 +1302,8 @@ void tst_QArrayData::arrayOpsExtra()
     }
 
     // copyAppend (value version) - special case of copying self value
-    {
+    // (appending requires growth at end)
+    if (GrowthPosition == QArrayData::GrowsAtEnd) {
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         const auto testCopyAppendSelf = [&] (auto &dataPointer, size_t n, const auto &value) {
             const size_t originalSize = dataPointer.size;
@@ -1337,7 +1349,8 @@ void tst_QArrayData::arrayOpsExtra()
     }
 
     // moveAppend
-    {
+    // (appending requires growth at end)
+    if (GrowthPosition == QArrayData::GrowsAtEnd) {
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         // now there's only one version that accepts "T*" as input parameters
         const auto testMoveAppend = [&] (auto &dataPointer, const auto &source)
@@ -1385,7 +1398,8 @@ void tst_QArrayData::arrayOpsExtra()
     }
 
     // moveAppend - special case of moving from self (this is legal yet rather useless)
-    {
+    // (appending requires growth at end)
+    if (GrowthPosition == QArrayData::GrowsAtEnd) {
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         const auto testMoveAppendSelf = [&] (auto &dataPointer, auto first, auto last) {
             const size_t originalSize = dataPointer.size;
@@ -1568,10 +1582,17 @@ void tst_QArrayData::arrayOpsExtra()
         CountedObject::LeakChecker localLeakChecker; Q_UNUSED(localLeakChecker);
         auto [intData, strData, objData] = setupDataPointers(inputSize * 2, inputSize / 2);
 
-        // make no free space at the begin
-        intData->insert(0, intData.freeSpaceAtBegin(), intData.data()[0]);
-        strData->insert(0, strData.freeSpaceAtBegin(), strData.data()[0]);
-        objData->insert(0, objData.freeSpaceAtBegin(), objData.data()[0]);
+        if (GrowthPosition == QArrayData::GrowsAtEnd) {
+            // make no free space at the begin
+            intData->insert(0, intData.freeSpaceAtBegin(), intData.data()[0]);
+            strData->insert(0, strData.freeSpaceAtBegin(), strData.data()[0]);
+            objData->insert(0, objData.freeSpaceAtBegin(), objData.data()[0]);
+        } else {
+            // make no free space at the end
+            intData->copyAppend(intData.freeSpaceAtEnd(), intData.data()[0]);
+            strData->copyAppend(strData.freeSpaceAtEnd(), strData.data()[0]);
+            objData->copyAppend(objData.freeSpaceAtEnd(), objData.data()[0]);
+        }
 
         // make all values unique. this would ensure that we do not have erroneously passed test
         int i = 0;
@@ -1580,12 +1601,21 @@ void tst_QArrayData::arrayOpsExtra()
         std::generate(objData.begin(), objData.end(), [] () { return CountedObject(); });
 
         // sanity checks:
-        QVERIFY(intData.freeSpaceAtEnd() > 0);
-        QVERIFY(strData.freeSpaceAtEnd() > 0);
-        QVERIFY(objData.freeSpaceAtEnd() > 0);
-        QVERIFY(intData.freeSpaceAtBegin() == 0);
-        QVERIFY(strData.freeSpaceAtBegin() == 0);
-        QVERIFY(objData.freeSpaceAtBegin() == 0);
+        if (GrowthPosition == QArrayData::GrowsAtEnd) {
+            QCOMPARE_GT(intData.freeSpaceAtEnd(), 0);
+            QCOMPARE_GT(strData.freeSpaceAtEnd(), 0);
+            QCOMPARE_GT(objData.freeSpaceAtEnd(), 0);
+            QCOMPARE(intData.freeSpaceAtBegin(), 0);
+            QCOMPARE(strData.freeSpaceAtBegin(), 0);
+            QCOMPARE(objData.freeSpaceAtBegin(), 0);
+        } else {
+            QCOMPARE(intData.freeSpaceAtEnd(), 0);
+            QCOMPARE(strData.freeSpaceAtEnd(), 0);
+            QCOMPARE(objData.freeSpaceAtEnd(), 0);
+            QCOMPARE_GT(intData.freeSpaceAtBegin(), 0);
+            QCOMPARE_GT(strData.freeSpaceAtBegin(), 0);
+            QCOMPARE_GT(objData.freeSpaceAtBegin(), 0);
+        }
 
         // now, prepend to full size causing the data to move internally. passed
         // value that refers to the object itself must be used correctly